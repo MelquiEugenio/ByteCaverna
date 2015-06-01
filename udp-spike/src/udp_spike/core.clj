ce51936899160081d07653c6fb57ed667573e32b
Melqui <melqui.eug@gmail.com>
2015-06-01 17:42:35 -0300

    implementing packet-to-receiver

--- udp-spike/src/udp_spike/core.clj
+++ udp-spike/src/udp_spike/core.clj
@@ -1,8 +1,8 @@
 (ns udp-spike.core)
 
 (defn create-xmitter [max-block-size content-bytes]
-  {:max-block-size max-block-size
-   :content-bytes content-bytes
+  {:max-block-size      max-block-size
+   :content-bytes       content-bytes
    :next-packet-to-send 0})
 
 (defn create-receiver [max-block-size]
@@ -10,7 +10,11 @@
    :last-block-received 0})
 
 (defn packet-to-receiver [xmitter]
-  (xmitter :content-bytes))
+  (let [packet (xmitter :content-bytes)
+        limit (xmitter :max-block-size)]
+    (if (< (alength packet) limit)
+      (byte-array (conj (vec packet) 127))
+      ())))
 
 (defn receiver-handle [receiver packet]
   {:content-bytes packet})
@@ -49,4 +53,4 @@
   (testa-transmissao "12345678")
   (testa-transmissao "123456789")
   (testa-transmissao "1234567890")
-  (testa-transmissao "12345678901"))
+  (testa-transmissao "1234567891"))
