6fab4db0375cd863c6d2883b53253c9ac70fde79
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-06-02 15:13:46 -0300

    Code Reorganization

--- udp-spike/src/udp_spike/core.clj
+++ udp-spike/src/udp_spike/core.clj
@@ -1,44 +1,53 @@
 (ns udp-spike.core)
 
-(defn create-xmitter [max-packet-size content-bytes]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; TRANSMITTER:
+
+(defn init-xmitter-state [max-packet-size content-bytes]
   {:max-packet-size   max-packet-size
    :content-bytes     content-bytes
    :next-block-to-send 0})
 
-(defn create-receiver [max-packet-size]
-  {:max-packet-size max-packet-size
-   :last-block-received 0})
-
-(defn packet-to-receiver [xmitter]
-  (let [packet (xmitter :content-bytes)
-        limit (xmitter :max-packet-size)]
+(defn packet-to-receiver [xmitter-state]
+  (let [packet (xmitter-state :content-bytes)
+        limit (xmitter-state :max-packet-size)]
     (if (< (alength packet) limit)
       (byte-array (conj (vec packet) 127))
       ())))
 
-(defn receiver-handle [receiver packet]
+(defn xmitter-handle [xmitter-state packet-from-receiver]
+  (update-in xmitter-state [:next-block-to-send] inc))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; RECEIVER:
+
+(defn init-receiver-state [max-packet-size]
+  {:max-packet-size     max-packet-size
+   :last-block-received 0})
+
+(defn receiver-handle [receiver-state packet-from-xmitter]
+  ;(assoc receiver-state ...)
   {:content-bytes packet})
 
-(defn packet-to-xmitter [receiver]
+(defn packet-to-xmitter [receiver-state]
   nil)
 
-(defn xmitter-handle [xmitter packet]
-  (update-in xmitter [:next-block-to-send] inc))
+(defn contents-received [receiver-state]
+  (receiver-state :content-bytes))
 
-(defn contents-received [receiver]
-  (receiver :content-bytes))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; TESTE
 
 (defn testa-transmissao-bytes [max-packet-size content-bytes]
   (let [result
-        (loop [receiver (create-receiver max-packet-size)
-               xmitter (create-xmitter max-packet-size content-bytes)]
-          (if-let [packet (packet-to-receiver xmitter)]
+        (loop [receiver-state (init-receiver-state max-packet-size)
+               xmitter-state (init-xmitter-state max-packet-size content-bytes)]
+          (if-let [packet-to-receiver (packet-to-receiver xmitter-state)]
             (do
               (assert (<= (alength packet) max-packet-size))
               (recur
-                (receiver-handle receiver packet)
-                (xmitter-handle xmitter (packet-to-xmitter receiver))))
-            (contents-received receiver)))]
+                (receiver-handle receiver-state packet)
+                (xmitter-handle xmitter-state (packet-to-xmitter receiver-state))))
+            (contents-received receiver-state)))]
     (assert (= result content-bytes))))
 
 (defn testa-transmissao [string]
